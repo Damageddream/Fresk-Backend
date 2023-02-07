1a99759fdd6c6c2eb0b76e1bd957ca75ac5d9d67
unknown <marcan.grab@gmail.com>
2023-02-07 09:49:45 +0100

    setting up backup and git repo, to work on deplo with https

--- backend/settings.py
+++ backend/settings.py
@@ -83,12 +83,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'coredb',
-        'USER': 'core',
-        'PASSWORD': '12345678',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
